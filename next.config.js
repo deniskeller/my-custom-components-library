e96a3547e86ec1584adeca8615b0547784e83468
deniskeller <dentean@mail.ru>
2025-03-18 00:19:59 +0400

    fix dependencies problem

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const path = require("path");
-const withImages = require("next-images");
+const path = require('path');
+const withImages = require('next-images');
 
 module.exports = withImages({
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "src/assets/scss")],
+    includePaths: [path.join(__dirname, 'src/assets/scss')],
     prependData: `@import "main.scss";`,
-  }  
+  },
 });
