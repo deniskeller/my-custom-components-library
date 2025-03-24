c3f78b93e673ff9623b4d7f523b71a14738a45f5
deniskeller <dentean@mail.ru>
2025-03-24 18:37:03 +0400

    update

--- src/view/components/Input/Input.tsx
+++ src/view/components/Input/Input.tsx
@@ -4,13 +4,11 @@ import {
   BaseInputNumber,
   BaseInputNumber2,
   BaseText,
-  BaseTextarea,
+  BaseTextarea
 } from '@base/index';
 import { LinkToViewCode } from '@nav/index';
 import React from 'react';
 import styles from './Input.module.scss';
-
-interface Props {}
 
 interface IValue {
   text1: string;
@@ -27,7 +25,7 @@ interface IValue {
   percent: number;
 }
 
-const Input: React.FC<Props> = () => {
+const Input: React.FC = () => {
   const [value, setValue] = React.useState<IValue>({
     text1: '',
     text2: '',
@@ -40,116 +38,116 @@ const Input: React.FC<Props> = () => {
     default: 0,
     phone: '',
     currency: 0,
-    percent: 0,
+    percent: 0
   });
 
   const setNewValue = (val: string | number, key: string) => {
-    setValue((prev) => ({ ...prev, [key]: val }));
+    setValue(prev => ({ ...prev, [key]: val }));
   };
 
   return (
     <>
       <div className={styles.Inputs}>
         <BaseContainer>
-          <BaseText className="Mb20">Input</BaseText>
+          <BaseText className='Mb20'>Input</BaseText>
 
-          <div className="Headline Mb20">
+          <div className='Headline Mb20'>
             <LinkToViewCode
-              title="Basic usage example."
-              href="https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseInput"
+              title='Basic usage example.'
+              href='https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseInput'
             />
           </div>
           <BaseInput
-            name="password"
-            placeholder="Basic usage"
-            type="text"
+            name='password'
+            placeholder='Basic usage'
+            type='text'
             value={value.text1}
             onChange={(val: string) => setNewValue(val, 'text1')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
 
           <BaseInput
-            label="Label"
-            name="text"
-            placeholder="Basic usage"
-            type="text"
+            label='Label'
+            name='text'
+            placeholder='Basic usage'
+            type='text'
             value={value.text2}
             onChange={(val: string) => setNewValue(val, 'text2')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
 
           <BaseInput
-            label="Input with error"
-            name="text"
-            error="Some text of error"
-            placeholder="Basic usage"
-            type="text"
+            label='Input with error'
+            name='text'
+            error='Some text of error'
+            placeholder='Basic usage'
+            type='text'
             value={value.text3}
             onChange={(val: string) => setNewValue(val, 'text3')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
 
           <BaseInput
-            label="Input password"
-            name="password"
-            placeholder="Input password"
-            type="password"
-            icon="eye-off"
+            label='Input password'
+            name='password'
+            placeholder='Input password'
+            type='password'
+            icon='eye-off'
             value={value.password}
             onChange={(val: string) => setNewValue(val, 'password')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
 
           <BaseInput
-            label="Input with icon left"
-            name="text"
-            placeholder="Basic usage"
-            type="text"
-            icon="save"
-            iconPosition="left"
+            label='Input with icon left'
+            name='text'
+            placeholder='Basic usage'
+            type='text'
+            icon='save'
+            iconPosition='left'
             value={value.text4}
             onChange={(val: string) => setNewValue(val, 'text4')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
 
           <BaseInput
-            label="Input with icon right"
-            name="text"
-            placeholder="Basic usage"
-            type="text"
-            icon="user"
-            iconPosition="right"
+            label='Input with icon right'
+            name='text'
+            placeholder='Basic usage'
+            type='text'
+            icon='user'
+            iconPosition='right'
             value={value.user}
             onChange={(val: string) => setNewValue(val, 'user')}
-            className="Mb20 mw300"
+            className='Mb20 mw300'
           />
         </BaseContainer>
 
         <BaseContainer>
-          <div className="Headline Mb20">
+          <div className='Headline Mb20'>
             <LinkToViewCode
-              title="Input number"
-              href="https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseInputNumber"
+              title='Input number'
+              href='https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseInputNumber'
             />
           </div>
 
           <BaseInputNumber
-            label="Default input number"
-            placeholder="Input number"
+            label='Default input number'
+            placeholder='Input number'
             value={value.default}
-            type="number"
+            type='number'
             onChange={(val: number) => setNewValue(val, 'default')}
-            className="mw300"
+            className='mw300'
           />
 
           <BaseInputNumber
-            label="Input phone"
-            className="mw300"
-            placeholder="+7(999) 999-99-99"
+            label='Input phone'
+            className='mw300'
+            placeholder='+7(999) 999-99-99'
             value={value.phone}
-            name="phone"
+            name='phone'
             onChange={(val: number) => setNewValue(val, 'phone')}
-            formatter={(value) => {
+            formatter={value => {
               return (
                 value
                   .toString()
@@ -164,14 +162,14 @@ const Input: React.FC<Props> = () => {
           />
 
           <BaseInputNumber
-            label="Input with currency"
-            placeholder="Input number"
+            label='Input with currency'
+            placeholder='Input number'
             value={value.currency}
             onChange={(val: number) => setNewValue(val, 'currency')}
-            className="mw300"
+            className='mw300'
             step={0.5}
             max={200000}
-            formatter={(value) => {
+            formatter={value => {
               return (
                 value.toString().replace(/\D/g, '').replace(/(\d)/, '$ $1') ||
                 '$ '
@@ -180,23 +178,23 @@ const Input: React.FC<Props> = () => {
           />
 
           <BaseInputNumber
-            label="Input with percent"
-            placeholder="Input number"
+            label='Input with percent'
+            placeholder='Input number'
             value={value.percent}
             onChange={(val: number) => setNewValue(val, 'percent')}
-            className="mw300"
-            formatter={(value) => `${value}%`}
+            className='mw300'
+            formatter={value => `${value}%`}
           />
 
           <BaseInputNumber2
-            label="Input with currency2"
-            placeholder="Input number"
+            label='Input with currency2'
+            placeholder='Input number'
             value={value.currency}
             onChange={(val: number) => setNewValue(val, 'currency')}
-            className="mw300"
+            className='mw300'
             step={0.5}
             max={100000}
-            formatter={(value) => {
+            formatter={value => {
               return (
                 value.toString().replace(/\D/g, '').replace(/(\d)/, '$ $1') ||
                 '$ '
@@ -208,30 +206,30 @@ const Input: React.FC<Props> = () => {
 
       <div className={styles.Textareas}>
         <BaseContainer>
-          <div className="Headline Mb20">
+          <div className='Headline Mb20'>
             <LinkToViewCode
-              title="TextArea."
-              href="https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseTextarea"
+              title='TextArea.'
+              href='https://github.com/deniskeller/my-custom-components-library/tree/main/src/components/base/BaseTextarea'
             />
           </div>
 
-          <BaseText className="Mb20">For multi-line input.</BaseText>
+          <BaseText className='Mb20'>For multi-line input.</BaseText>
 
           <BaseTextarea
-            className="mw400"
-            placeholder="Some text"
+            className='mw400'
+            placeholder='Some text'
             value={value.textarea1}
             onChange={(val: string) => setNewValue(val, 'textarea1')}
           />
         </BaseContainer>
         <BaseContainer>
-          <BaseText className="Mb20">With character counting</BaseText>
+          <BaseText className='Mb20'>With character counting</BaseText>
 
           <BaseTextarea
-            placeholder="Some text"
+            placeholder='Some text'
             showCount
             maxLength={100}
-            label="Label for textarea with counter"
+            label='Label for textarea with counter'
             value={value.textarea2}
             onChange={(val: string) => setNewValue(val, 'textarea2')}
           />
